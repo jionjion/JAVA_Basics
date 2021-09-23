28f1bedc9134f121d76985d2cc3b32315da47188
jionjion <jionjion@aliyun.com>
2021-09-23 09:27:09 +0800

    2021-09-23

--- JavaBase/src/main/java/concurrency/controller/ThreadLocalController.java
+++ JavaBase/src/main/java/concurrency/controller/ThreadLocalController.java
@@ -6,16 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * 	调用线程的信息
+ * 调用线程的信息
+ *
+ * @author Jion
  */
 @Controller
-@RequestMapping(path="/threadLocal")
+@RequestMapping(path = "/threadLocal")
 public class ThreadLocalController {
 
-	/** 使用线程信息获得每个线程独有的参数 */
-	@RequestMapping(path="/getInfo")
-	@ResponseBody
-	public String getInfo() {
-		return ThreadInfo.getInfo();
-	}
+    /**
+     * 使用线程信息获得每个线程独有的参数
+     */
+    @RequestMapping(path = "/getInfo")
+    @ResponseBody
+    public String getInfo() {
+        return ThreadInfo.getInfo();
+    }
 }
